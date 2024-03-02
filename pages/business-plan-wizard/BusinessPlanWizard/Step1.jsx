cd8287c35654f0009cf19355e98907e1c52cd538
Hyq0719 <2418011730@qq.com>
2024-03-02 19:02:16 +0000

    BusinessPlanWriter

--- pages/business-plan-wizard/BusinessPlanWizard/Step1.jsx
+++ pages/business-plan-wizard/BusinessPlanWizard/Step1.jsx
@@ -57,68 +57,72 @@ const Step1 = ({nextStep}) => {
     );
 
     return (
-        <Grid container direction="column" spacing={2} component="form" noValidate autoComplete="off">
-            <Grid item>
-                <FormControl component="fieldset" error={error.businessType} fullWidth>
-                    <Typography variant="subtitle1" component="legend" sx={{mb: 1, fontWeight: 'bold'}}>
-                        {requiredLabel('Is this an existing or an upcoming business?')}
-                    </Typography>
-                    <RadioGroup
-                        name="businessType"
-                        value={step1FormData.businessType || ''}
-                        onChange={handleChange}
-                    >
-                        <StyledFormControlLabel value="existing" groupValue={step1FormData.businessType}
-                                                 label="Existing business"/>
-                        <StyledFormControlLabel value="upcoming" groupValue={step1FormData.businessType}
-                                                 label="Upcoming unlaunched business"/>
-                    </RadioGroup>
-                    {error.businessType && <FormHelperText>Please select an option.</FormHelperText>}
-                </FormControl>
-            </Grid>
-
-            <Grid item>
-                <FormControl component="fieldset" error={error.planUsage} fullWidth>
-                    <Typography variant="subtitle1" component="legend" sx={{mb: 1, fontWeight: 'bold'}}>
-                        {requiredLabel('What will you use this business plan for?')}
-                    </Typography>
-                    <RadioGroup
-                        name="planUsage"
-                        value={step1FormData.planUsage || ''}
-                        onChange={handleChange}
-                    >
-                        <StyledFormControlLabel value="investors" groupValue={step1FormData.planUsage}
-                                                 label="To be used to request fund from investors"/>
-                        <StyledFormControlLabel value="banks" groupValue={step1FormData.planUsage}
-                                                 label="To be used to request fund from banks"/>
-                        <StyledFormControlLabel value="selfLearning" groupValue={step1FormData.planUsage}
-                                                 label="For self-learning"/>
-                        <StyledFormControlLabel value="schoolProject" groupValue={step1FormData.planUsage}
-                                                 label="To be used as reference for school/university project"/>
-                        <StyledFormControlLabel value="workplaceProject" groupValue={step1FormData.planUsage}
-                                                 label="To be used as reference for workplace projects"/>
-                        <StyledFormControlLabel value="nonFinancial" groupValue={step1FormData.planUsage}
-                                                 label="To be used to submit to a non-financial institution for approval"/>
-                        <StyledFormControlLabel value="other" groupValue={step1FormData.planUsage}
-                                                 label="Other reasons"/>
-                    </RadioGroup>
-                    {error.planUsage && <FormHelperText>Please select an option.</FormHelperText>}
-                </FormControl>
-            </Grid>
-
-            <Grid container justifyContent="flex-start" spacing={2} sx={{mt: 2}}>
+        <form noValidate autoComplete="on" onSubmit={onFinish}>
+            <Grid container direction="column" spacing={2} component="form" noValidate autoComplete="off">
                 <Grid item>
-                    <Button disabled>
-                        <KeyboardArrowLeft/> Back
-                    </Button>
+                    <FormControl component="fieldset" error={error.businessType} fullWidth>
+                        <Typography variant="subtitle1" component="legend" sx={{mb: 1, fontWeight: 'bold'}}>
+                            {requiredLabel('Is this an existing or an upcoming business?')}
+                        </Typography>
+                        <RadioGroup
+                            id="businessType"
+                            name="businessType"
+                            value={step1FormData.businessType || ''}
+                            onChange={handleChange}
+                        >
+                            <StyledFormControlLabel value="existing" groupValue={step1FormData.businessType}
+                                                    label="Existing business"/>
+                            <StyledFormControlLabel value="upcoming" groupValue={step1FormData.businessType}
+                                                    label="Upcoming unlaunched business"/>
+                        </RadioGroup>
+                        {error.businessType && <FormHelperText>Please select an option.</FormHelperText>}
+                    </FormControl>
                 </Grid>
+
                 <Grid item>
-                    <Button onClick={onFinish}>
-                        Next <KeyboardArrowRight/>
-                    </Button>
+                    <FormControl component="fieldset" error={error.planUsage} fullWidth>
+                        <Typography variant="subtitle1" component="legend" sx={{mb: 1, fontWeight: 'bold'}}>
+                            {requiredLabel('What will you use this business plan for?')}
+                        </Typography>
+                        <RadioGroup
+                            id="planUsage"
+                            name="planUsage"
+                            value={step1FormData.planUsage || ''}
+                            onChange={handleChange}
+                        >
+                            <StyledFormControlLabel value="investors" groupValue={step1FormData.planUsage}
+                                                    label="To be used to request fund from investors"/>
+                            <StyledFormControlLabel value="banks" groupValue={step1FormData.planUsage}
+                                                    label="To be used to request fund from banks"/>
+                            <StyledFormControlLabel value="selfLearning" groupValue={step1FormData.planUsage}
+                                                    label="For self-learning"/>
+                            <StyledFormControlLabel value="schoolProject" groupValue={step1FormData.planUsage}
+                                                    label="To be used as reference for school/university project"/>
+                            <StyledFormControlLabel value="workplaceProject" groupValue={step1FormData.planUsage}
+                                                    label="To be used as reference for workplace projects"/>
+                            <StyledFormControlLabel value="nonFinancial" groupValue={step1FormData.planUsage}
+                                                    label="To be used to submit to a non-financial institution for approval"/>
+                            <StyledFormControlLabel value="other" groupValue={step1FormData.planUsage}
+                                                    label="Other reasons"/>
+                        </RadioGroup>
+                        {error.planUsage && <FormHelperText>Please select an option.</FormHelperText>}
+                    </FormControl>
+                </Grid>
+
+                <Grid container justifyContent="flex-start" spacing={2} sx={{mt: 2}}>
+                    <Grid item>
+                        <Button disabled>
+                            <KeyboardArrowLeft/> Back
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <Button onClick={onFinish}>
+                            Next <KeyboardArrowRight/>
+                        </Button>
+                    </Grid>
                 </Grid>
             </Grid>
-        </Grid>
+        </form>
     );
 };
 
