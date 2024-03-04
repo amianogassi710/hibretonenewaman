757ce3c381c54661f7e56872e6e00e2e01f2570a
Hyq0719 <2418011730@qq.com>
2024-03-04 22:56:31 +0000

    UserLogin fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,27 @@
 import "../public/assets/css/style.css";
 import "../styles/globals.css";
-import { useEffect } from "react";
+import {useEffect} from "react";
+import {useLocalStorage} from "react-use";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
+    const [isLoggedIn, setIsLoggedIn] = useLocalStorage("is_logged_in", false);
+    const [userAccount, setUserAccount] = useLocalStorage("user_account", {});
     useEffect(() => {
         require("../public/assets/js/bootstrap.bundle.min.js");
     }, []);
+
+    useEffect(() => {
+        if (isLoggedIn) {
+            fetch("/auth/login/jwt", {
+                method: "POST",
+            }).then((response) => {
+                if (response.status !== 200) {
+                    setIsLoggedIn(false);
+                    setUserAccount({});
+                }
+            });
+        }
+    }, [])
 
     return <Component {...pageProps} />;
 }
