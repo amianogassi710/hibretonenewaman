9a67251383fd0e000e88fe88cb30b690eacadd2d
Rakshita Yadav <rakshitayadav@Rakshitas-MacBook-Air.local>
2024-03-11 14:10:43 +0000

    added banner for scholarship page

---
+++ pages/page-scholarship.js
@@ -0,0 +1,20 @@
+import Layout from "../components/Layout/Layout";
+import TestimonialSlider1 from "./../components/sliders/Testimonial1";
+
+export default function Scholarship() {
+    return (
+        <>
+            <Layout>
+                <section>
+                    <div className="banner-area">
+                        <div className="banner-image"></div>
+                        <p className="header1">Entrepreneurial Scholarships & Bursaries</p>
+                        <p className="header2"> Enabling the kickstart of entrepreneurial careers for all aspirers, with finance, resource and mentorship support </p>
+                        <div className="blue-line"></div>
+                        <div className="blue-line-2"></div>
+                    </div>
+                </section>
+            </Layout>
+        </>
+    );
+}
