dfb7f2d948b65336d7a78dcedd5d659032e1b10f
Hyq0719 <2418011730@qq.com>
2024-03-19 18:32:07 +0000

    Update BusinessPlanWriter Generate Page

--- pages/business-plan-wizard/BusinessPlanWizard/BusinessPlanStepForm.jsx
+++ pages/business-plan-wizard/BusinessPlanWizard/BusinessPlanStepForm.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import {useSessionStorage} from 'react-use';
+import React, {useState} from 'react';
 import {Typography, Stepper, Step, StepLabel, Grid, IconButton, Box, MobileStepper} from '@mui/material';
-import Link from "next/link";
 import {ArrowBack} from "@mui/icons-material";
 import dynamic from "next/dynamic";
+import {useRouter} from "next/router";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+import {useSessionStorage} from 'react-use';
 
 const NoSSRStep1 = dynamic(() => import('./Step1'), {ssr: false})
 const NoSSRStep2 = dynamic(() => import('./Step2'), {ssr: false})
@@ -13,7 +19,7 @@ const NoSSRStep5 = dynamic(() => import('./Step5'), {ssr: false})
 const NoSSRStep6 = dynamic(() => import('./Step6'), {ssr: false})
 const NoSSRStep7 = dynamic(() => import('./Step7'), {ssr: false})
 const NoSSRStep8 = dynamic(() => import('./Step8'), {ssr: false})
-// const NoSSRBizPlan = dynamic(() => import('./BizPlan'), {ssr: false})
+const NoSSRBizPlan = dynamic(() => import('./BizPlan'), {ssr: false})
 
 const BusinessPlanStepForm = () => {
     const [currentStepStr, setCurrentStepStr] = useSessionStorage('currentStep', '1');
@@ -21,6 +27,37 @@ const BusinessPlanStepForm = () => {
 
     const nextStep = () => setCurrentStepStr(String(currentStep + 1));
     const previousStep = () => setCurrentStepStr(String(currentStep - 1));
+    const [open, setOpen] = useState(false);
+    const router = useRouter();
+
+    const handleBackClick = () => {
+        if (currentStep > 8) {
+            setOpen(true);
+        } else {
+            router.push("/business-plan-writer");
+        }
+    };
+
+    const handleCloseDialog = (goHome = false) => {
+        setOpen(false);
+        if (goHome) {
+            router.push("/business-plan-writer");
+        }
+    };
+
+    const removeAllBusinessPlan = () => {
+        Object.keys(sessionStorage).forEach((key) => {
+            if (key.startsWith('BusinessPlanStepForm' || key.startsWith('BusinessPlan'))) {
+                sessionStorage.removeItem(key);
+            }
+        });
+    };
+    React.useEffect(() => {
+        if (currentStep > 8) {
+            removeAllBusinessPlan();
+            setCurrentStepStr('1');
+        }
+    }, []);
 
     const stepsItems = ['Business Plan Objective', 'Basic Business Information', 'Customer Group Details', 'Product or Service Details', 'Success Drivers', 'Initial Investment Details', 'Financial Details',];
 
@@ -35,19 +72,42 @@ const BusinessPlanStepForm = () => {
                       margin: '0 auto'
                   }}>
                 <Box sx={{width: 30, mb: 1}}>
-                    <Link legacyBehavior href="/business-plan-writer">
-                        <IconButton sx={{
+                    <IconButton
+                        onClick={handleBackClick}
+                        sx={{
                             transform: 'scale(1.25)',
                             color: 'primary.main',
-                        }}>
-                            <ArrowBack/>
-                        </IconButton>
-                    </Link>
+                        }}
+                    >
+                        <ArrowBack/>
+                    </IconButton>
                 </Box>
                 <Typography variant="h6" sx={{flexGrow: 2, textAlign: 'center', mb: 1}}>
                     Business Plan Writer
                 </Typography>
                 <Box sx={{width: 30}}/>
+            </Grid>
+            <Grid item xs={12} md={12} lg={6}>
+                <Dialog
+                    open={open}
+                    onClose={() => handleCloseDialog()}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"Go Back Home?"}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            If you go back now, the Business Plan you're creating will not be saved. Are you sure you
+                            want to leave?
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={() => handleCloseDialog()}>Cancel</Button>
+                        <Button onClick={() => handleCloseDialog(true)} autoFocus>
+                            Back
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </Grid>
             {currentStep <= 7 && (
                 <Grid item lg={12} justifyContent="center"
@@ -104,7 +164,7 @@ const BusinessPlanStepForm = () => {
                 {currentStep === 6 && <NoSSRStep6 nextStep={nextStep} previousStep={previousStep}/>}
                 {currentStep === 7 && <NoSSRStep7 nextStep={nextStep} previousStep={previousStep}/>}
                 {currentStep === 8 && <NoSSRStep8 nextStep={nextStep} previousStep={previousStep}/>}
-                {/*{currentStep === 9 && <NoSSRBizPlan/>}*/}
+                {currentStep === 9 && <NoSSRBizPlan/>}
             </Grid>
         </Grid>
     );
